7bf5d441199e2f252024ac9f503a235159d74b0d
Pedro Ramirez <prlara2000@gmail.com>
2024-02-16 17:37:44 -0600

    fix scroll to top button

--- script.js
+++ script.js
@@ -6,8 +6,8 @@ document.addEventListener('DOMContentLoaded', function () {
   function scrollFunction() {
     // Check if the screen width is greater than 1055px
     if (window.innerWidth > 1055) {
-      let scrollButtonDesktop = document.getElementById('top');
-      if (document.body.scrollTop > 400 || document.documentElement.scrollTop > 400) {
+      let scrollButtonDesktop = document.getElementById('scroller');
+      if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
         scrollButtonDesktop.style.display = 'block';
       } else {
         scrollButtonDesktop.style.display = 'none';
@@ -15,7 +15,7 @@ document.addEventListener('DOMContentLoaded', function () {
     } else {
       // If screen width is not greater than 1055px, always hide the button
       let scrollButtonMobile = document.getElementById('scroller');
-      if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+      if (document.body.scrollTop > 400 || document.documentElement.scrollTop > 400) {
         scrollButtonMobile.style.display = 'block';
       } else {
         scrollButtonMobile.style.display = 'none';
